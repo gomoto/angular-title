0712741d3aa363c0f8e3a181c5ae40ba6829d61c
Ryan Gomoto <ryan@cyrusbio.com>
2016-11-28 01:05:14 -0800

    Replace browser-run + tap-finished with tape-run

--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,17 @@
 const browserify = require('browserify');
-const browserRun = require('browser-run');
 const faucet = require('faucet');
 const glob = require('glob');
-const tapFinished = require('tap-finished');
+const tapeRun = require('tape-run');
 const tsify = require('tsify');
 
 const paths = require('./paths');
 
-const browserRunner = browserRun();
-
-const b = browserify({
+browserify({
   debug: true
 })
 .add(glob.sync(paths.tests))
 .plugin(tsify, { project: paths.tsconfig })
 .bundle()
-.pipe(browserRunner);
-
-b.pipe(tapFinished((results) => {
-  browserRunner.stop();
-}))
-
-b.pipe(faucet())
-.pipe(process.stdout)
+.pipe(tapeRun())
+.pipe(faucet())
+.pipe(process.stdout);
