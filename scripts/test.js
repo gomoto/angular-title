3d08fbd8f33ebb66927888a6c841fe8342c3c2a5
Ryan Gomoto <ryan@cyrusbio.com>
2016-11-27 23:40:52 -0800

    Stop browser after tests finish

--- scripts/test.js
+++ scripts/test.js
@@ -2,16 +2,24 @@ const browserify = require('browserify');
 const browserRun = require('browser-run');
 const faucet = require('faucet');
 const glob = require('glob');
+const tapFinished = require('tap-finished');
 const tsify = require('tsify');
 
 const paths = require('./paths');
 
-browserify({
+const browserRunner = browserRun();
+
+const b = browserify({
   debug: true
 })
 .add(glob.sync(paths.tests))
 .plugin(tsify, { project: paths.tsconfig })
 .bundle()
-.pipe(browserRun())
-.pipe(faucet())
-.pipe(process.stdout);
+.pipe(browserRunner);
+
+b.pipe(tapFinished((results) => {
+  browserRunner.stop();
+}))
+
+b.pipe(faucet())
+.pipe(process.stdout)
