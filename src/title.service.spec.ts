5ac6f71ffaadfd36752db294d94b630d8fbdaf88
Ryan Gomoto <ryan@cyrusbio.com>
2016-11-28 00:11:57 -0800

    Mock $window service with a simple object

--- src/title.service.spec.ts
+++ src/title.service.spec.ts
@@ -1,16 +1,12 @@
 import * as test from 'blue-tape';
-import * as angular from 'angular';
 import TitleService from './title.service';
 
-/**
- * Create a new instance of angular injector.
- * We cannot use angular.mock.inject, which
- * is only allowed when using jasmine or mocha.
- */
 const setup = () => {
-  // create a new injector in strict-di mode
-  const $injector = angular.injector(['ng'], true);
-  const $window = $injector.get<ng.IWindowService>('$window');
+  const $window = <ng.IWindowService> {
+    document: {
+      title: ''
+    }
+  };
   const $title = new TitleService($window);
   return {
     $title,
